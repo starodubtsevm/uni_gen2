3d7fc2f00115ff063b04a259d6073a600ee48ffd
Starodubtsev Maksim <starodubtsevm@gmail.com>
2020-03-12 18:10:49 +0500

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from classes import *
+
+#---------------------------------------------------------------------------
+#afc1 = afc_device(amplitude,frequency,blocksize,samplerate,
+#	freq_min,freq_max,freq_step,time_conv)
+afc1 = afc_device()
+
+input("...Press Enter to exit...")
+#---------------------------------------------------------------------------
